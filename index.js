a4881fc9f050b22a6bae505176d53c427b6a5269
Matthew <matthewwgalloway@gmail.com>
2017-03-18 20:24:44 -0500

    Write functions to parse body into JSON
    Wrote a Pet constructor function, and a parsePet function that
    returns a JSON collection of pets. Added cheerio dependency.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var http = require('http');
+var cheerio = require('cheerio')
 var url = 'http://ws.petango.com/webservices/adoptablesearch/wsAdoptableAnimals.aspx?species=Cat&sex=A&agegroup=All&onhold=A&orderby=ID&colnum=3&AuthKey=srx1fd0hqaa8hw78rv752eypne2t6bykvf0u8do2b2hvkkvrf7'
-
 
 module.exports.handler = (event, context, callback) => {
   
@@ -35,8 +35,26 @@ module.exports.handler = (event, context, callback) => {
   
   })
   
+var Pet = function(petCell){
+  var $petCell = $(petCell);
+  this.id = $petCell.find(".list-animal-id").text(),
+  this.image_url = $petCell.find(".list-animal-photo").attr("src"),
+  this.name = $petCell.find(".list-animal-name").text(),
+  this.species = $petCell.find(".list-anima-species").text(),
+  this.sex = $petCell.find(".list-animal-sexSN").text(),
+  this.breed = $petCell.find(".list-animal-breed").text(),
+  this.age = $petCell.find(".list-animal-age").text();
+}
   
-  
+function parsePets(body){
+  var $ = cheerio.load(body)
+  var petCells = $('td').toArray();
+  petArray = petCells.map(function(obj){ return new Pet(obj);});
+  return JSON.stringify(petArray);
+}
+
+
+
     // callback(null, response);
   // }
 

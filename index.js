76954cc0c15111bc8292505c8c9d158d07f5f178
Matthew <matthewwgalloway@gmail.com>
2017-03-18 21:39:22 -0500

    Debug parsing functions, add to end event
    Debugged errors in the parsing functions and added them to the end
    event. It currently serves all cats serialized to JSON.

--- index.js
+++ index.js
@@ -2,40 +2,9 @@
 
 var http = require('http');
 var cheerio = require('cheerio')
-var url = 'http://ws.petango.com/webservices/adoptablesearch/wsAdoptableAnimals.aspx?species=Cat&sex=A&agegroup=All&onhold=A&orderby=ID&colnum=3&AuthKey=srx1fd0hqaa8hw78rv752eypne2t6bykvf0u8do2b2hvkkvrf7'
+var url = 'http://ws.petango.com/webservices/adoptablesearch/wsAdoptableAnimals.aspx?species=Cat&sex=A&agegroup=All&onhold=A&orderby=ID&colnum=3&AuthKey=srx1fd0hqaa8hw78rv752eypne2t6bykvf0u8do2b2hvkkvrf7';
 
-module.exports.handler = (event, context, callback) => {
-  
-  // const response = {
-  //   statusCode: 200,
-    
-    
-  //   body: JSON.stringify({
-  //     message: 'Go Serverless v1.0! Your function executed successfully!',
-  //     input: event,
-  //   }),
-  
-  // };
-  
-  http.get(url, function(res){
-    var body = '';  
-    res.on('data', function(data){
-      body += data;
-    });
-
-    res.on('end', function(){
-      console.log('Works?');
-      callback(null, body);
-    });  
-    
-    res.on('error', function(e){
-      console.log("Didn't work");
-      context.done(null, 'FAILURE');
-    })
-  
-  })
-  
-var Pet = function(petCell){
+var Pet = function(petCell, $){
   var $petCell = $(petCell);
   this.id = $petCell.find(".list-animal-id").text(),
   this.image_url = $petCell.find(".list-animal-photo").attr("src"),
@@ -46,20 +15,35 @@ var Pet = function(petCell){
   this.age = $petCell.find(".list-animal-age").text();
 }
   
-function parsePets(body){
-  var $ = cheerio.load(body)
+function parsePets(body, $){
   var petCells = $('td').toArray();
-  petArray = petCells.map(function(obj){ return new Pet(obj);});
-  return JSON.stringify(petArray);
+  var petArray = petCells.map(function(obj){ return new Pet(obj, $);});
+  return {
+    statusCode: 200,
+    headers: { contentType: "application/json" },
+    body: JSON.stringify(petArray)
+  };
 }
 
-
-
-    // callback(null, response);
-  // }
-
+module.exports.handler = (event, context, callback) => {
   
+  http.get(url, function(res){
+    var body = '';  
+    res.on('data', function(data){
+      body += data;
+    });
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+    res.on('end', function(){
+      var $ = cheerio.load(body)
+      console.log('Works?');
+      var response = parsePets(body, $);
+      callback(null, response);
+    });  
+    
+    res.on('error', function(e){
+      console.log("Didn't work");
+      context.done(null, 'FAILURE');
+    })
+  
+  })
 };
